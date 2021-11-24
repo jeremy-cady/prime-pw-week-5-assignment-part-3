5f5d4863c56503d128a11b5d4149180362e541fc
jeremy-cady <jeremy.cady@outlook.com>
2021-11-24 17:08:47 -0600

    showCollection function written and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,11 @@
 console.log('***** Music Collection *****')
 
+// create empty array named 'collection'
 let collection = [];
 
-function addToCollection(title, artist, yearPublished) {
-    console.log('In addToCollection');
+// write function to add album objects to the 'collection' array 
+function addToCollection( title, artist, yearPublished ) {
+    console.log('In addToCollection', title, artist, yearPublished);
 
     let newAlbum = {
         title: title,
@@ -15,6 +17,7 @@ function addToCollection(title, artist, yearPublished) {
     return newAlbum;
 }
 
+// adding six albums to 'collection' array
 console.log('Adding to collection:', addToCollection('Djesse Vol. 3', 'Jacob Collier', '2020'));
 console.log('Adding to collection:', addToCollection('SuperBlue', 'Kurt Elling', '2021'));
 console.log('Adding to collection:', addToCollection('Turbo', 'Cory Wong & Dirty Loops', '2021'));
@@ -22,6 +25,21 @@ console.log('Adding to collection:', addToCollection('Phoenix', 'Dirty Loops', '
 console.log('Adding to collection:', addToCollection('Reverie', 'Tom Misch', '2016'));
 console.log('Adding to collection:', addToCollection('Liquid Spirit', 'Gregory Porter', '2013'));
 
+// console.log collection array
+console.log(collection);
+
+// write a function called 'showCollection' to display 'collection' array
+function showCollection( array ) {
+    console.log('In showCollection', array);
+
+    console.log('Number of albums in the collection:', array.length);
+
+    for (let element of array) {
+        console.log(`${element.title} by ${element.artist}, published in ${element.year}`);   
+    }
+}
+
+showCollection(collection);
 
 
 
