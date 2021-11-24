2afad94485b662b70baf82b3d0dbcaad1779e4f3
jeremy-cady <jeremy.cady@outlook.com>
2021-11-24 16:55:34 -0600

    wrote addToCollection function and added six albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,27 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    console.log('In addToCollection');
+
+    let newAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(newAlbum);
+
+    return newAlbum;
+}
+
+console.log('Adding to collection:', addToCollection('Djesse Vol. 3', 'Jacob Collier', '2020'));
+console.log('Adding to collection:', addToCollection('SuperBlue', 'Kurt Elling', '2021'));
+console.log('Adding to collection:', addToCollection('Turbo', 'Cory Wong & Dirty Loops', '2021'));
+console.log('Adding to collection:', addToCollection('Phoenix', 'Dirty Loops', '2020'));
+console.log('Adding to collection:', addToCollection('Reverie', 'Tom Misch', '2016'));
+console.log('Adding to collection:', addToCollection('Liquid Spirit', 'Gregory Porter', '2013'));
+
+
+
+
